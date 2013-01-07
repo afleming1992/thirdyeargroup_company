5f81b13f557c55f6c59c7d6c6841f9c2024ee9b9
sr217 <stuartretson91@gmail.com>
2013-01-07 17:48:44 +0000

    changed background colour to dark blue

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html>
+     
+<head> 
+        <meta charset=utf-8> 
+        <title>Third Year Project</title>
+        
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"> 
+        <link href="css/main.css" rel="stylesheet" type="text/css"> 
+        <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css" /> 
+        <script src="http://code.jquery.com/jquery-1.8.2.js"></script> 
+        <script src="http://code.jquery.com/ui/1.9.1/jquery-ui.js"></script> 
+        <script src="bootstrap/js/bootstrap.min.js"></script> 
+</head>
 
+    <body>
+        Hello World
+        <div id ="container">
+        
+             <div id ="banner">
+            </div>
+                <div id ="mainContent">
+                    <div id ="nav">
+                     
+                        
+                  </div>
+                    <div id ="content">
+                        
+                  </div>
+            </div>
+        </div>
+    </body>
+</html>
