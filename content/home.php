db6b9fc956a0f3489070fc17cf51a898b7b5e94e
Andrew Fleming <ajf9@hw.ac.uk>
2013-01-10 23:42:13 +0000

    Further Styling Changes and started to construct a Homepage

--- content/home.php
+++ content/home.php
@@ -1,8 +1,6 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-<b>Home</b>
+<div class="hero-unit">
+	<h1>Welcome</h1>
+	<p>Technological Developments is an unique, forward-thinking, proffesional team of Software Developers and Business Analysists who have a passion for creating great software</p>
+	<button class='btn btn-info btn-large'>More About us</button>
+</div>
+<p>We believe in the importance of what great software can do for an business and our aim is to help businesses work more effectively using the software we create.</p>
--- index.php
+++ index.php
@@ -23,11 +23,21 @@
             <div class="navbar" style='margin-bottom:0px;width:1010px'>
                 <div class="navbar-inner">
                       <ul class="nav">
-                         <li class="active"><a href="?file=home">Home</a></li>
-                         <li><a href="?file=aboutus">About Us</a></li>
-                         <li><a href="?file=products">Products</a></li>
-                         <li><a href="?file=projects">Projects</a></li>
-                         <li><a href="?file=contactus">Contact Us</a></li>
+						<?php
+							if(!isset($_GET['file']))
+							{
+								$file = "home";
+							}
+							else
+							{
+								$file = htmlspecialchars($_GET['file']);
+							}
+						?>
+						<li <?php if($file == "home"){print("class='active'");} ?>><a href="?file=home"><i class="icon-home"></i> Home</a></li>
+						<li <?php if($file == "aboutus"){print("class='active'");} ?>><a href="?file=aboutus"><i class="icon-question-sign"></i> About Us</a></li>
+						<li <?php if($file == "team"){print("class='active'");} ?>><a href="?file=team"><i class="icon-user"></i> Meet the Team</a></li>
+						<li <?php if($file == "projects"){print("class='active'");} ?>><a href="?file=projects"><i class="icon-folder-open"></i> Projects</a></li>
+                        <li <?php if($file == "contactus"){print("class='active'");} ?>><a href="?file=contactus"><i class="icon-envelope"></i></i> Contact Us</a></li>
                       </ul>
                     </div>
                 </div>
@@ -53,7 +63,9 @@
 							?>
 						</div>
 						<div class='sidecontentbox'>
-							Testing
+						<?php
+							include("content/sidebar.php")
+						?>
 						</div>
 				</div>
             </div>
