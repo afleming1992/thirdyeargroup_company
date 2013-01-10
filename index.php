4638ee83ec797261c27b2b91fef62ef77173f4a8
sr217 <stuartretson91@gmail.com>
2013-01-10 21:16:15 +0000

    Navbar, spans and css updates (in prog)

--- index.php
+++ index.php
@@ -20,26 +20,54 @@
 				 <img src='images/logo.png' />
 			 </div>
 		</div>
+            <div class="navbar span12">
+                
+                        <div class="navbar-inner">
+                           
+                         <ul class="nav">
+                         <li class="active"><a href="?file=home">Home</a></li>
+                         <li><a href="?file=aboutus">About Us</a></li>
+                         <li><a href="?file=products">Products</a></li>
+                         <li><a href="?file=projects">Projects</a></li>
+                         <li><a href="?file=contactus">Contact Us</a></li>
+                        </ul>
+                            
+                        </div>
+                   </div>
 		<div class='row'>
              <div class='contentbox span12'>
-				<div class='row'>
-					<div class='span8'>
+             		<div class='row'>
+             			<div class='span8 mainContentBox'>
+             				
+             <?php
+             	 if(!isset($_GET['file']))
+                                      {
+                                              $file = "content/home.php";
+                                      }
+                              else
+                                      {
+                                              $file = "content/". $_GET['file'] . ".php";
+                                      }               
+                              if(file_exists("$file"))
+                                      {
+                                              include("$file");
+                                      }
+                              else
+                                      {
+                                              include("content/error.php");
+                                      }
+						?>
+				
+   
+					
 					
 					</div>
-					<div class='span4'>
-					
+					<div class='span3 mainContentBox'>
+					Testing
 					</div>
 				</div>
              </div>
 		</div>
-                <div id="mainContent">
-                    <div id="nav">
-                     
-                        
-                  </div>
-                    <div id="content">
-                        
-                  </div>
             </div>
         </div>
     </body>
