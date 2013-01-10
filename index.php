453d919742f51671772cee68a8642daac83f60e8
Andrew Fleming <ajf9@hw.ac.uk>
2013-01-10 22:37:56 +0000

    Major Structural and Styling Changes and removing files we don't need

--- index.php
+++ index.php
@@ -15,59 +15,50 @@
 
     <body>
         <div class='container'>
-		<div class='row'>
-             <div class='banner span12'>
-				 <img src='images/logo.png' />
-			 </div>
-		</div>
-            <div class="navbar span12">
-                
-                        <div class="navbar-inner">
-                           
-                         <ul class="nav">
+			<div class='row' style='margin-left:auto;margin-right:auto;'>
+				<div class='banner'>
+					<img src='images/banner2.png' />
+				</div>
+			</div>
+            <div class="navbar" style='margin-bottom:0px;width:1010px'>
+                <div class="navbar-inner">
+                      <ul class="nav">
                          <li class="active"><a href="?file=home">Home</a></li>
                          <li><a href="?file=aboutus">About Us</a></li>
                          <li><a href="?file=products">Products</a></li>
                          <li><a href="?file=projects">Projects</a></li>
                          <li><a href="?file=contactus">Contact Us</a></li>
-                        </ul>
-                            
-                        </div>
-                   </div>
-		<div class='row'>
-             <div class='contentbox span12'>
-             		<div class='row'>
-             			<div class='span8 mainContentBox'>
-             				
-             <?php
-             	 if(!isset($_GET['file']))
-                                      {
-                                              $file = "content/home.php";
-                                      }
-                              else
-                                      {
-                                              $file = "content/". $_GET['file'] . ".php";
-                                      }               
-                              if(file_exists("$file"))
-                                      {
-                                              include("$file");
-                                      }
-                              else
-                                      {
-                                              include("content/error.php");
-                                      }
-						?>
-				
-   
-					
-					
-					</div>
-					<div class='span3 mainContentBox'>
-					Testing
-					</div>
+                      </ul>
+                    </div>
+                </div>
+             <div class='contentbox'>
+             		<div class='mainContentBox'>
+             				<?php
+									if(!isset($_GET['file']))
+                                    {
+										$file = "content/home.php";
+                                    }
+									else
+                                    {
+                                        $file = "content/". $_GET['file'] . ".php";
+                                    }               
+									if(file_exists("$file"))
+                                    {
+                                        include("$file");
+                                    }
+									else
+                                    {
+                                        include("content/error.php");
+                                    }
+							?>
+						</div>
+						<div class='sidecontentbox'>
+							Testing
+						</div>
 				</div>
-             </div>
-		</div>
+            </div>
+            <div class='footer'>
+				Copyright &copy; Technological Developments 2013
             </div>
         </div>
     </body>
