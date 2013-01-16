8c1519d3c453b0b240e88fa31dd13d59f8a45a54
Andrew Fleming <ajf9@hw.ac.uk>
2013-01-16 21:51:49 +0000

    Added About us page

---
+++ content/aboutus.php
@@ -0,0 +1,7 @@
+<h1>About us</h1>
+<p>Technological Developments are a small software company just starting out. We are located on the outskirts of Edinburgh with a small office situated in the Heriot Watt University Research Park. We are a team of seven professionals who work together to achieve one common goal, customer satisfaction. 
+</p>
+<p>
+Our main aim is to provide our customers with the best possible end product. To achieve this we tailor our products to the exact needs of our customer by making sure we have regular meetings in order to gain knowledge and understanding of their requirements and receive feedback throughout the development process. 
+</p>
+<p>If you wish to contact us for assistance with purchasing any of our products or commission a software development project, click the button below!</p>
